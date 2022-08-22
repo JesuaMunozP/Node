71c4e9ed5ccbe7dd52892057c70215e7874da068
JesuaMunozP <jesuammp@gmail.com>
2022-08-22 17:47:35 -0400

    Add Service Tests

--- src/products/test/products.service.spec.ts
+++ src/products/test/products.service.spec.ts
@@ -3,254 +3,211 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Product } from '../entities/product.entity';
 import { ProductsService } from '../products.service';
 import { ProductImage } from '../entities/product-image.entity';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, Repository, QueryRunner, QueryBuilder } from 'typeorm';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { HttpException } from '@nestjs/common';
+import e from 'express';
+import { UpdateProductDto } from '../dto/update-product.dto';
 
 const objQuery = {
-  connect: jest.fn(),
-  startTransaction: jest.fn(),
-  rollbackTransaction: jest.fn(),
-  release: jest.fn(),
+    connect: jest.fn(),
+    startTransaction: jest.fn(),
+    rollbackTransaction: jest.fn(),
+    commitTransaction: jest.fn(),
+    release: jest.fn(),
 };
+
+let queryRunner: QueryRunner;
+
 describe('ProductService', () => {
-  let service: ProductsService;
-  let productImage: Repository<ProductImage>;
+    let service: ProductsService;
+    let productImage: Repository<ProductImage>;
 
-  const mockProductRepository = {
-    create: jest.fn().mockImplementation((dto) => dto),
-    save: jest
-      .fn()
-      .mockImplementation((product) =>
-        Promise.resolve({ id: Date.now(), ...product }),
-      ),
-    findOneBy: jest
-      .fn()
-      .mockImplementation((product) => Promise.resolve({ id: Date.now() })),
-    findOne: jest
-      .fn()
-      .mockImplementation((product) => Promise.resolve({ id: Date.now() })),
-    update: jest.fn(),
-    find: jest.fn(),
-    preload: jest.fn(),
-    remove: jest.fn(),
+    const mockProductRepository = {
+        create: jest.fn().mockImplementation((dto) => dto),
+        save: jest.fn().mockImplementation((product) => Promise.resolve({ id: Date.now(), ...product }),),
+        findOneBy: jest.fn().mockImplementation((product) => Promise.resolve({ id: Date.now() })),
+        findOne: jest.fn().mockImplementation((product) => Promise.resolve({ id: Date.now() })),
+        //findOne: jest.fn().mockImplementation((product) => ({title: 'title'})),
+        update: jest.fn(),
+        find: jest.fn(),
+        preload: jest.fn(),
+        remove: jest.fn(),
+        findOnePlain: jest.fn().mockImplementation((dto) => dto)
+    };
 
-    // update: jest.fn(),
-  };
+    const mockDataSourceRepository = {
+        createQueryBuilder: jest.fn(),
+        createQueryRunner: jest.fn().mockImplementation(() => objQuery),
+    };
 
-  const mockDataSourceRepository = {
-    createQueryBuilder: jest.fn(),
-    createQueryRunner: jest.fn().mockImplementation(() => objQuery),
-  };
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                ProductsService,
+                {
+                    provide: getRepositoryToken(Product),
+                    useValue: mockProductRepository,
+                },
+                {
+                    provide: getRepositoryToken(ProductImage),
+                    useValue: mockProductRepository,
+                },
+                {
+                    provide: DataSource,
+                    useValue: mockDataSourceRepository,
+                },
+            ],
+        }).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        ProductsService,
-        {
-          provide: getRepositoryToken(Product),
-          useValue: mockProductRepository,
-        },
-        {
-          provide: getRepositoryToken(ProductImage),
-          useValue: mockProductRepository,
-        },
-        {
-          provide: DataSource,
-          useValue: mockDataSourceRepository,
-        },
-      ],
-    }).compile();
-
-    service = module.get<ProductsService>(ProductsService);
-    productImage = module.get<Repository<ProductImage>>(
-      getRepositoryToken(ProductImage),
-    );
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  it('should create a new product and return', async () => {
-    expect(await service.create({ title: 'Test' })).toEqual({
-      images: [],
-      title: 'Test',
+        service = module.get<ProductsService>(ProductsService);
+        productImage = module.get<Repository<ProductImage>>(
+            getRepositoryToken(ProductImage),
+        );
     });
-  });
 
-  it('should return a product', async () => {
-    expect(
-      await service.findOne('033ae036-6a20-4760-a11c-f83667474085'),
-    ).toEqual({
-      id: expect.any(Number),
+    it('should be defined', () => {
+        expect(service).toBeDefined();
     });
-  });
 
-
-  describe('should create a new product and return', () => {
     it('should create a new product and return', async () => {
-      
-      const product = new CreateProductDto();
-      product.title = 'title';
-      product.price = 200;
-      product.description = 'description';
-      product.slug = 'slug';
-      product.stock = 10;
-      product.sizes = ['s', 'm'];
-      product.gender = 'gender';
-      product.tags = [];
-      product.images = ['1', '2', '3'];
+        expect(await service.create({ title: 'Test' })).toEqual({
+            images: [],
+            title: 'Test',
+        });
+    });
 
-      // mockProductRepository.save.mockReturnValue(product);
-      mockProductRepository.create.mockReturnValue(product);
+    it('should return a product', async () => {
+        expect(
+            await service.findOne('033ae036-6a20-4760-a11c-f83667474085'),
+        ).toEqual({
+            id: expect.any(Number),
+        });
+    });
 
-      const saveProduct = await service.create(product);
+    describe('should create a new product and return', () => {
+        it('should create a new product and return', async () => {
 
-      expect(saveProduct).toMatchObject(product);
+            const product = new CreateProductDto();
+            product.title = 'title';
+            product.price = 200;
+            product.description = 'description';
+            product.slug = 'slug';
+            product.stock = 10;
+            product.sizes = ['s', 'm'];
+            product.gender = 'gender';
+            product.tags = [];
+            product.images = ['1', '2', '3'];
+
+            mockProductRepository.create.mockReturnValue(product);
+
+            const saveProduct = await service.create(product);
+
+            expect(saveProduct).toMatchObject(product);
+        });
+
+        it('should return a exception when doesnt create a product', async () => {
+            await expect(service.create(null)).rejects.toThrowError(
+                HttpException,
+            );
+        });
     });
 
     it('should return a exception when doesnt create a product', async () => {
-        await expect(service.create(null)).rejects.toThrowError(
-            HttpException,
-          );
-    });
-  });
+        await service.findOne(null).catch((e) => {
+            expect(e);
+        });
 
-  describe('When search All Products', () => {
-    it('should be list of all products', async () => {
-      const paginationDto = {
-        limit: 1,
-        offset: 1,
-      };
-
-      const product = new CreateProductDto();
-      product.title = 'title';
-      product.price = 200;
-      product.description = 'description';
-      product.slug = 'slug';
-      product.stock = 10;
-      product.sizes = ['s', 'm'];
-      product.gender = 'gender';
-      product.tags = [];
-      product.images = ['1', '2', '3'];
-      mockProductRepository.find.mockReturnValue([product, product]);
-      const products = await service.findAll(paginationDto);
-      expect(products).toHaveLength(2);
-    });
-  });
-
-  describe('When delete a product', () => {
-    it('Should delete a existing product', async () => {
-      const productDelete = {
-        id: '033ae036-6a20-4760-a11c-f83667474085',
-        title: 'title',
-        price: 200,
-        description: 'description',
-        slug: 'slug',
-        stock: 10,
-        sizes: ['s', 'm'],
-        gender: 'gender',
-        tags: [],
-        images: ['1', '2', '3'],
-      };
-
-      mockProductRepository.findOne.mockReturnValue(productDelete);
-      const productDeleted = await service.remove(productDelete.id);
-
-      expect(mockProductRepository.findOne).toBeDefined();
-    });
-  });
-
-  it('Should update a user', async () => {
-    /* const product = {
-            id: '033ae036-6a20-4760-a11c-f83667474085',
-            title: 'title',
-            price: 200,
-            description: 'description',
-            slug: 'slug',
-            stock: 10,
-            sizes: ['s','m'],
-            gender: 'gender',
-            tags: [],
-            images: ['1','2','3'],
-        }*/
-    const product = new CreateProductDto();
-    product.title = 'title';
-    product.price = 200;
-    product.description = 'description';
-    product.slug = 'slug';
-    product.stock = 10;
-    product.sizes = ['s', 'm'];
-    product.gender = 'gender';
-    product.tags = [];
-    product.images = ['1', '2', '3'];
-
-    const productUpdate = {
-      title: 'titleUpdate',
-    };
-
-    mockProductRepository.preload.mockReturnValue(product);
-    mockProductRepository.update.mockReturnValue({
-      ...product,
-      ...productUpdate,
     });
 
-    mockProductRepository.create.mockReturnValue({
-      ...product,
-      ...productUpdate,
+    describe('When search All Products', () => {
+        it('should be list of all products', async () => {
+            const paginationDto = {
+                limit: 1,
+                offset: 1,
+            };
+
+            const product = new CreateProductDto();
+            product.title = 'title';
+            product.price = 200;
+            product.description = 'description';
+            product.slug = 'slug';
+            product.stock = 10;
+            product.sizes = ['s', 'm'];
+            product.gender = 'gender';
+            product.tags = [];
+            product.images = ['1', '2', '3'];
+            await mockProductRepository.find.mockReturnValue([product, product]);
+            const products = await service.findAll(paginationDto);
+            expect(products).toHaveLength(2);
+        });
     });
 
-    // const resultProduct = await service.update(
-    //   '033ae036-6a20-4760-a11c-f83667474085',
-    //   {
-    //     ...product,
-    //     title: 'titleUpdate',
-    //   },
-    // );
-  });
+    describe('When delete a product', () => {
+        it('Should delete a existing product', async () => {
+            const productDelete = {
+                id: '033ae036-6a20-4760-a11c-f83667474085',
+                title: 'title',
+                price: 200,
+                description: 'description',
+                slug: 'slug',
+                stock: 10,
+                sizes: ['s', 'm'],
+                gender: 'gender',
+                tags: [],
+                images: ['1', '2', '3'],
+            };
 
-  it('update() should return error', async () => {
-    const product = new CreateProductDto();
-    product.title = 'title';
-    product.price = 200;
-    product.description = 'description';
-    product.slug = 'slug';
-    product.stock = 10;
-    product.sizes = ['s', 'm'];
-    product.gender = 'gender';
-    product.tags = [];
-    product.images = ['1', '2', '3'];
+            await mockProductRepository.findOne.mockReturnValue(productDelete);
+            const productDeleted = await service.remove(productDelete.id);
 
-    const productUpdate = {
-      title: 'titleUpdate',
-    };
-
-    mockProductRepository.preload.mockReturnValue(product);
-    mockProductRepository.update.mockReturnValue({
-      ...product,
-      ...productUpdate,
+            expect(await mockProductRepository.findOne).toBeDefined();
+        });
     });
 
-    mockProductRepository.create.mockReturnValue({
-      ...product,
-      ...productUpdate,
+    describe('Update a product', () => {
+        it('update() should return error', async () => {
+            const product = new CreateProductDto();
+            product.title = 'title';
+            product.price = 200;
+            product.description = 'description';
+            product.slug = 'slug';
+            product.stock = 10;
+            product.sizes = ['s', 'm'];
+            product.gender = 'gender';
+            product.tags = [];
+            product.images = ['1', '2', '3'];
+
+            const productUpdate = {
+                title: 'titleUpdate',
+            };
+
+            await mockProductRepository.preload.mockReturnValue(product);
+
+            await mockProductRepository.update.mockReturnValue({
+                ...product,
+                ...productUpdate,
+            });
+
+            await mockProductRepository.create.mockReturnValue({
+                ...product,
+                ...productUpdate,
+            });
+
+        });
     });
 
-    // const resultProduct = await service.update(
-    //   '033ae036-6a20-4760-a11c-f83667474085',
-    //   {
-    //     ...product,
-    //     title: 'titleUpdate',
-    //   },
-    // );
-  });
+    it('should expect an error trying to update a product', async () => {
+        await service.update('033ae036-6a20-4760-a11c-f83667474085', { title: 'Test' }).catch((e) => {
+            expect(e);
+        });
+    });
 
-  it('should expect an error trying to update a product', async () => {
-    await service
-      .update('033ae036-6a20-4760-a11c-f83667474085', { title: 'Test' })
-      .catch((e) => {
-        expect(e);
-      });
-  });
+    it('findOnePlain() should return a product using findOnePlain', async () => {
+        expect(await service.findOnePlain('033ae036-6a20-4760-a11c-f83667474085')).toEqual({
+            id: expect.any(Number),
+            images: [],
+        });
+    });
+
 });
